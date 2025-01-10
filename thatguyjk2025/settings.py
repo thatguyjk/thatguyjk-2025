d94fb14bbc919479aa290c0214c768f125565cf8
Jonathan Kelly <jonathan.kelly@instrument.com>
2025-01-10 10:55:25 -0500

    adding a call to load_dotenv function in settings

--- thatguyjk2025/settings.py
+++ thatguyjk2025/settings.py
@@ -13,9 +13,11 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 
+# load environment variables from .env file
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
